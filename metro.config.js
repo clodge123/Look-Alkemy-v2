8e327c9270e883593fcdddcd7db49719c0615463
Clodge123 <clodge123@users.noreply.github.com>
2026-07-15 22:38:09 -0500

    Remove metro.config.js - let Expo handle it

--- metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Ensure all JS files are properly resolved
-config.resolver.sourceExts = ['js', 'jsx', 'ts', 'tsx'];
-
-module.exports = config;
